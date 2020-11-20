3a78df7062186bb036d7627bfad8b418e05a9188
Ayush Shrestha <am.shrestha@kmg.com.np>
2020-11-20 16:18:50 +0545

    vue first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div id="dtr-wrapper" >  
+      <RwvHeader />
+      <router-view></router-view>
+      <RwvFooter />
+    </div>
+  </div>
+</template>
+
+<script>
+import RwvHeader from "@/components/TheHeader";
+import RwvFooter from "@/components/TheFooter";
+
+export default {
+  name: "App",
+  components: {
+    RwvHeader,
+    RwvFooter
+  }
+};
+</script>
+
+
---
+++ src/main.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+
+import { CHECK_AUTH } from "./store/actions.type";
+import ApiService from "./common/api.service";
+import DateFilter from "./common/date.filter";
+import ErrorFilter from "./common/error.filter";
+
+Vue.config.productionTip = false;
+Vue.filter("date", DateFilter);
+Vue.filter("error", ErrorFilter);
+
+ApiService.init();
+
+// Ensure we checked auth before each page load.
+router.beforeEach((to, from, next) =>
+  Promise.all([store.dispatch(CHECK_AUTH)]).then(next)
+);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
+
+import "./assets/css/bootstrap.min.css";
+import "./assets/fonts/iconfonts.css";
+// import "./assets/css/plugins.css"; 
+import "./assets/css/style.css"; 
+import "./assets/css/responsive.css";
+import "./assets/css/color.css";
+import "./assets/js/jquery.min.js"; 
+// import "./assets/js/bootstrap.min.js"; 
+// import "./assets/js/plugins.js"; 
+import "./assets/js/custom.js";
